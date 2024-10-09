a0f8ed78164add4be4bdc584a84777b74c142f45
yuramishin <robothot@yandex.ru>
2024-10-09 15:54:42 +0300

    оптимизировал sum order

--- files_and_data_structures/main.py
+++ files_and_data_structures/main.py
@@ -1,4 +1,5 @@
 import re
+from utils import timer
 def write_copy_file(i_path: str, o_path: str) -> None:
     """Функция копирует данные из одного файла и вставляет во второй
     :param i_path: input path, пусть файла с исходными данными
@@ -9,15 +10,25 @@ def write_copy_file(i_path: str, o_path: str) -> None:
         with open(o_path, 'w', encoding='utf-8') as o_file:
             o_file.write(i_file.read())
 
+@timer
 def sum_order(path: str) -> int:
     """Функция считает сумму заказа из файла.
     :param path: Пусть до файла
     :return: Сумма заказа
     """
-    with open(path, 'r', encoding='utf-8') as file:
-        result = sum(map(int, re.findall(r'\d+\n', file.read())))
-        return result
-
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            result = 0
+            for data in file:
+                try:
+                    count, price = map(int, data.strip().split()[1:])
+                except ValueError:
+                    print('Убедитесь, что поля count и price не пустые и содержат корректные значения')
+                else:
+                    result += count * price
+            return result
+    except FileNotFoundError:
+        print('Проверьте путь до файла')
 
 def counter_words(path: str) -> None:
     """Функция считает количество слов в файле
@@ -39,10 +50,10 @@ def search_unique_line(i_path: str, o_path: str) -> None:
             o_file.write(*data)
 
 def main():
-    write_copy_file('data/source.txt', 'results/destination.txt')
+    # write_copy_file('data/source.txt', 'results/destination.txt')
     print(sum_order('data/prices.txt'))
-    counter_words('data/text_file.txt')
-    search_unique_line('data/input.txt', 'results/unique_output.txt')
+    # counter_words('data/text_file.txt')
+    # search_unique_line('data/input.txt', 'results/unique_output.txt')
 
 if __name__ == '__main__':
     main()
