bb91fefbdcb248d41ab18f7d307590e87fd690da
yuramishin <robothot@yandex.ru>
2024-10-09 13:03:11 +0300

    Добавил 4 задание, внес правки во 2 задание

--- files_and_data_structures/main.py
+++ files_and_data_structures/main.py
@@ -19,19 +19,31 @@ def sum_order(path: str) -> int:
 @timer
 def sum_order2(path: str) -> int:
     with open(path, 'r', encoding='utf-8') as file:
-        result = sum(list(map(int, re.findall(r'\d+\n', file.read()))))
+        result = sum(map(int, re.findall(r'\d+\n', file.read())))
         return result
 
 @timer
-def counter_words(path: str) -> int:
+def counter_words(path: str) -> None:
     with open(path, 'r', encoding='utf-8') as file:
-        return len(re.findall(r'\w+.?', file.read()))
+        print(len(re.findall(r'\w+.?', file.read())))
+
+def search_unique_line(i_path: str, o_path: str) -> None:
+    """Функция копирует данные из одного файла и вставляет уникальные строки во второй
+    :param i_path: input path, пусть файла с исходными данными
+    :param o_path: output path, пусть файла с полученными данными
+    :return: None. Результат записывается в файл
+    """
+    with open(i_path, 'r', encoding='utf-8') as i_file:
+        with open(o_path, 'w', encoding='utf-8') as o_file:
+            data = {line for line in i_file.readlines()}
+            o_file.write(*data)
 
 def main():
     # write_copy_file('data/source.txt', 'results/destination.txt')
-    # print(sum_order('data/prices.txt'))
-    # print(sum_order2('data/prices.txt'))
-    counter_words('data/text_file.txt')
+    print(sum_order('data/prices.txt'))
+    print(sum_order2('data/prices.txt'))
+    # counter_words('data/text_file.txt')
+    # search_unique_line('data/input.txt', 'results/unique_output.txt')
 
 if __name__ == '__main__':
     main()
