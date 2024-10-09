dbcb84d0de0739836ca8880e07b1d5345975596c
yuramishin <robothot@yandex.ru>
2024-10-09 10:58:44 +0300

    Поменял структуру 1 задачи

--- files_and_data_structures/main.py
+++ files_and_data_structures/main.py
@@ -1,9 +1,16 @@
 
+def write_copy_file(i_path: str, o_path: str) -> None:
+    """Функция копирует данные из одного файла и вставляет во второй
+    :param i_path: input path, пусть файла с исходными данными
+    :param o_path: output path, пусть файла с полученными данными
+    :return: None. Результат записывается в файл
+    """
+    with open(i_path, 'r', encoding='utf-8') as i_file:
+        with open(o_path, 'w', encoding='utf-8') as o_file:
+            o_file.write(i_file.read())
 
 def main():
-    with open('data/source.txt', 'r', encoding='utf-8') as i_file:
-        with open('results/destination.txt', 'w', encoding='utf-8') as o_file:
-            o_file.write(i_file.read())
+    write_copy_file('data/source.txt', 'results/destination.txt')
 
 if __name__ == '__main__':
     main()
