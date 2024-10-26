d17a1d71b386496e6bb0743616e5773547de09e0
yuramishin <robothot@yandex.ru>
2024-10-26 15:29:00 +0300

    Добавил доп задание, пофиксил баг

--- classes_and_objects/main.py
+++ classes_and_objects/main.py
@@ -16,9 +16,9 @@ class Product:
         self._stock = count
 
     def update_stock(self, quantity: int):
-        if self.stock - quantity < 0:
+        if self.stock + quantity < 0:
             raise Exception('Недостаточно товара на складе')
-        self.stock -= quantity
+        self.stock += quantity
 
 class Order:
     def __init__(self):
@@ -26,11 +26,18 @@ class Order:
 
     def add_product(self, product: Product, quantity: int):
         try:
-            product.update_stock(quantity)
+            product.update_stock(-quantity)
         except Exception as ex:
             print(ex)
         else:
             self.order[product] = quantity
+
+    def remove_product(self, product: Product, quantity: int) -> None:
+        if self.order[product] - quantity <= 0:
+            self.order.pop(product)
+        else:
+            self.order[product] -= quantity
+        product.update_stock(quantity)
 
     def calculate_total(self):
         return sum(product.price * amount for product, amount in self.order.items())
@@ -51,31 +58,52 @@ class Store:
         order = Order()
         return order
 
+def info(product: Product, order: Order, action: str) -> None:
+    """Вывод данные о состоянии продукта и показателях заказа."""
+    print("Товар '{0}' - {1}:"
+          "\n-\tОстаток на складе: {2}"
+          "\n-\tОстаток в магазине: {3}"
+          "\n-\tОбщая стоимость заказа: {4}".format(
+        product,
+        action,
+        product.stock,
+        order.order.get(product, 0),
+        order.calculate_total()
+    ))
+
 def main():
     # Создаем магазин
     store = Store()
 
     # Создаем товары
-    product1 = Product("Ноутбук", 1000, 5)
-    product2 = Product("Смартфон", 500, 10)
+    prod_laptop = Product("Ноутбук", 1000, 5)
+    prod_smartphone = Product("Смартфон", 500, 10)
+    prod_tablet = Product("Планшет", 650, 8)
+    prod_headphones = Product("Наушники", 200, 15)
 
     # Добавляем товары в магазин
-    store.add_product(product1)
-    store.add_product(product2)
+    products = [prod_laptop, prod_smartphone, prod_tablet, prod_headphones]
+    for prod in products:
+        store.add_product(prod)
 
-    # Список всех товаров
+    # Список всех товаров магазине
     store.list_products()
 
     # Создаем заказ
     order = store.create_order()
+    info(prod_headphones, order, 'Создаем заказ')
 
     # Добавляем товары в заказ
-    order.add_product(product1, 2)
-    order.add_product(product2, 3)
+    for prod in products:
+        order.add_product(prod, prod.stock)
+    info(prod_headphones, order, 'Добавляем товары в заказ')
 
-    # Выводим общую стоимость заказа
-    total = order.calculate_total()
-    print(f"Общая стоимость заказа: {total}")
+    # Удаляем из заказа товар
+    order.remove_product(prod_headphones, 15)
+    info(prod_headphones, order, 'Удаляем из заказа товар')
+
+    order.remove_product(prod_laptop, 3)
+    info(prod_laptop, order, 'Удаляем из заказа товар')
 
     # Проверяем остатки на складе после заказа
     store.list_products()
